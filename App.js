d644634fb7052376de315e2e1ff30f1569f403ea
zixin <jsczzzx@yahoo.com>
2022-09-18 23:43:38 -0400

    sth

--- App.js
+++ App.js
@@ -1,38 +1,10 @@
-import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
-import AboutScreen from './Components/AboutScreen'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainApp from './src/MainApp';
 
-const MainRoute = () => <Text>Music</Text>;
-
-const StatisticsRoute = () => <Text>Recents</Text>;
-
-const AccountRoute = () => <Text>Accounts</Text>;
-
-const AboutRoute = () => <AboutScreen/>;
-
-const MyComponent = () => {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'main', title: 'Main', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
-    { key: 'statistics', title: 'Statistics', focusedIcon: 'chart-bar', unfocusedIcon: 'chart-bar-stacked' },
-    { key: 'account', title: 'Account', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
-    { key: 'about', title: 'About', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
-  ]);
-
-  const renderScene = BottomNavigation.SceneMap({
-    main: MainRoute,
-    statistics: StatisticsRoute,
-    account: AccountRoute,
-    about: AboutRoute,
-  });
-
+export default function App() {
   return (
-    <BottomNavigation
-      navigationState={{ index, routes }}
-      onIndexChange={setIndex}
-      renderScene={renderScene}
-    />
+    <MainApp/>
   );
-};
+}
 
-export default MyComponent;
---
+++ src/MainApp.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+import AboutScreen from './Screens/AboutScreen'
+
+const MainRoute = () => <Text>Music</Text>;
+
+const StatisticsRoute = () => <Text>Recents</Text>;
+
+const AccountRoute = () => <Text>Accounts</Text>;
+
+const AboutRoute = () => <AboutScreen/>;
+
+const MainApp = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'main', title: 'Main', focusedIcon: 'home', unfocusedIcon: 'home-outline'},
+    { key: 'statistics', title: 'Statistics', focusedIcon: 'chart-bar', unfocusedIcon: 'chart-bar-stacked' },
+    { key: 'account', title: 'Account', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
+    { key: 'about', title: 'About', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    main: MainRoute,
+    statistics: StatisticsRoute,
+    account: AccountRoute,
+    about: AboutRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
+
+export default MainApp;
