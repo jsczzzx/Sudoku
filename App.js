637de39328d6fb00b810f11ed063977e49c45845
zixin <jsczzzx@yahoo.com>
2022-09-19 01:19:14 -0400

    react-navigation ver 0.1

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 import MainApp from './src/MainApp';
+import Login from './src/Login';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const MyStack = () => {
   return (
-    <MainApp/>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+        }}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
+      />
+    </Stack.Navigator>
   );
 }
 
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
